f23e0db3ef1c3c31b138de54b67e8a754ba588df
Mohan <nxtwave@ip-10-2-0-76.ap-south-1.compute.internal>
2024-05-13 16:54:42 +0000

    Debbuged

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {Switch, BrowserRouter, Route} from 'react-router-dom'
+
+import Header from './components/Header'
+import About from './components/About'
+import Home from './components/Home'
+import Contact from './components/Contact'
+import BlogItemDetails from './components/BlogItemDetails'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <div className="app-container">
+    <div className="responsive-container">
+      <div className="app-body">
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+            <Route path="/blogs/:id" component={BlogItemDetails} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </div>
+  </div>
+)
+
+export default App
